edd296d494dc5ff8b771e037b54b6e2cd4eb2542
Mathieu <bon.mathieu@gmail.com>
2024-08-08 23:35:42 +0200

    Entire cleanup of project and restarting it through sqlalchemy tutorial

--- entities/Address.py
+++ entities/Address.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from entities import Base
 
 
-class Address(BaseModel):
-    street: str
-    city: str
-    zip: str
-    country: str
+class Address(Base):
+    __tablename__ = "address"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user_account.id"))
+
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email address={self.email_address!r})"
--- sqlalchemy_tutorial.py
+++ sqlalchemy_tutorial.py
@@ -1,30 +1,86 @@
 import random
 from typing import Any
-from sqlalchemy import CursorResult, Engine, create_engine, text
+from sqlalchemy import (
+    CursorResult,
+    Engine,
+    create_engine,
+    text,
+    MetaData,
+    Table,
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+)
+from sqlalchemy.orm import DeclarativeBase
 
 
-# engine: Engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-engine: Engine = create_engine("sqlite+pysqlite:///db_test.sqlite", echo=True)
+def create_table(engine: Engine) -> None:
+    with engine.begin() as conn:
+        # conn.execute(text("DELETE FROM some_table"))
+        conn.execute(text("CREATE TABLE IF NOT EXISTS some_table (x INT, y INT)"))
+    print("")
 
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT  'Hello big world!'"))
-    print(result.all())
-print("")
 
-with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE IF NOT EXISTS some_table (x INT, y INT)"))
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": random.randint(0, 200), "y": random.randint(0, 200)}],
+def delete_table_content(engine: Engine) -> None:
+    with engine.begin() as conn:
+        conn.execute(text("DELETE FROM some_table"))
+    print("")
+
+
+def insert_entries(engine: Engine) -> None:
+    with engine.begin() as conn:
+        conn.execute(
+            text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+            [{"x": random.randint(0, 200), "y": random.randint(0, 200)}],
+        )
+    print("")
+
+
+def print_results(engine: Engine) -> None:
+    with engine.connect() as conn:
+        result: CursorResult[Any] = conn.execute(
+            text("SELECT * FROM some_table WHERE x < :max_number"), {"max_number": 100}
+        )
+
+        for row in result:
+            print(f"x is {row.x} and y is {row.y}")
+
+    print("")
+
+
+def create_user_table(metadata_obj: MetaData) -> Table:
+    return Table(
+        "user_account",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(30)),
+        Column("fullname", String),
     )
-    conn.commit()
-print("")
 
 
-with engine.connect() as conn:
-    result: CursorResult[Any] = conn.execute(text("SELECT * FROM some_table"))
-    print(result.all())
-print("")
+def create_address_table(metadata_obj: MetaData) -> Table:
+    return Table(
+        "address",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("user_id", ForeignKey("user_account.id"), nullable=False),
+        Column("email_address", String, nullable=False),
+    )
 
 
 
+def main():
+    # engine: Engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+    engine: Engine = create_engine("sqlite+pysqlite:///database/concerts_db.sqlite", echo=True)
+    metadata_obj = MetaData()
+
+    # print_results(engine)
+
+    user_table = create_user_table(metadata_obj)
+    address_table = create_address_table(metadata_obj)
+    metadata_obj.create_all(engine)
+
+
+if __name__ == "__main__":
+    main()
