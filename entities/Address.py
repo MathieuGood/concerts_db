98277b314c066b907cf6cd1556d2b0277d8548d5
Mathieu <bon.mathieu@gmail.com>
2024-08-11 00:16:22 +0200

    Made association between Venue and Address. Deleted the User class and replaces UserRepository by PersonRepository

--- entities/Address.py
+++ entities/Address.py
@@ -1,20 +1,19 @@
-from sqlalchemy import ForeignKey
+from dataclasses import dataclass
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from entities.Base import Base
 
 
+@dataclass
 class Address(Base):
 
     __tablename__ = "address"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str]
-    user_id = mapped_column(ForeignKey("user_account.id"))
+    city: Mapped[str] = mapped_column(String)
+    country: Mapped[str] = mapped_column(String)
 
-    user: Mapped["User"] = relationship(back_populates="addresses")
-
-    def __repr__(self) -> str:
-        return f"Address(id={self.id!r}, email address={self.email_address!r})"
+    venue: Mapped["Venue"] = relationship("Venue", back_populates="address")
 
 
-from entities.User import User
+from entities.Venue import Venue
--- sqlalchemy_tutorial/metadata_table_objects.py
+++ sqlalchemy_tutorial/metadata_table_objects.py
@@ -11,7 +11,7 @@ from sqlalchemy import (
 
 def create_user_table(metadata_obj: MetaData) -> Table:
     return Table(
-        "user_account",
+        "user",
         metadata_obj,
         Column("id", Integer, primary_key=True),
         Column("name", String(30)),
@@ -24,7 +24,7 @@ def create_address_table(metadata_obj: MetaData) -> Table:
         "address",
         metadata_obj,
         Column("id", Integer, primary_key=True),
-        Column("user_id", ForeignKey("user_account.id"), nullable=False),
+        Column("user_id", ForeignKey("user.id"), nullable=False),
         Column("email_address", String, nullable=False),
     )
 
