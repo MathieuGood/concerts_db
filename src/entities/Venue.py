f60e9b771cc4d657552fb16319e46cae03101260
Mathieu <bon.mathieu@gmail.com>
2024-08-11 00:33:27 +0200

    Wrote relationship between Address and Venue but UNTESTED.

--- src/entities/Venue.py
+++ src/entities/Venue.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.entities.Base import Base
 
 
 @dataclass
 class Venue(Base):
-
     __tablename__ = "venues"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    
-    address_id = mapped_column(ForeignKey("addresses.id"), nullable=False)
+
+    address_id: Mapped[int] = mapped_column(ForeignKey("addresses.id"), nullable=False)
+    address: Mapped["Address"] = relationship("Address", back_populates="venues")
+
+
+from src.entities.Address import Address
