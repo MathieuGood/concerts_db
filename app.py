c7f95f1be6ff1a5b1c4c42daa1360c330e0f2711
Mathieu <bon.mathieu@gmail.com>
2024-08-11 00:16:22 +0200

    Made association between Venue and Address. Deleted the User class and replaces UserRepository by PersonRepository

--- app.py
+++ app.py
@@ -1,47 +1,62 @@
 from sqlalchemy import Engine, create_engine, MetaData, insert, text
 from entities.Base import Base
 from entities.User import User
+from entities.Address import Address
 from sqlalchemy.orm import Session
 
+from entities.repositories.PersonRepository import PersonRepository
 from mockup_data.characters import characters
 
+
 def delete_all_users(session: Session):
-    session.execute(text("DELETE FROM user_account"))
+    session.execute(text("DELETE FROM users"))
+    session.execute(text("DELETE FROM addresses"))
     session.commit()
 
 
 def main():
     # engine: Engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+    import os
+    os.remove("database/concerts_db.sqlite")
+
     engine: Engine = create_engine(
         "sqlite+pysqlite:///database/concerts_db.sqlite", echo=True
     )
     metadata_obj = MetaData()
-    session = Session(engine)
 
-    # Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
 
     # delete_all_users(session)
 
-    # for character in characters:
-    #     session.add(character)
-    # session.flush()
-    # session.commit()
+    user_repository = PersonRepository(session)
 
+    user_repository.add_multiple(characters)
 
-    # Edit the first record of the user_account table
-    # first_user = session.query(User).first()
-    # if first_user:
-    #     first_user.fullname = "New Name"
-    #     session.commit()
+    kenny: User = user_repository.get_by_id(4)
+    stan: User = user_repository.get_by_id(3)
+
+    kenny_address = Address(city="South Park", country="USA")
+    stan_address = Address(city="Tegridyville", country="USA")
+    kenny.addresses.append(kenny_address)
+    stan.addresses.append(stan_address)
+
+    print("")
+    print(kenny.addresses)
+    print("")
+    print(stan.addresses)
+
+    session.commit()
 
     # eric = session.get(User, 1)
     # print(eric.fullname)
 
-    all_users = session.query(User).all()
-    print(f"Type of all_users: {type(all_users)}")
-    for user in all_users:
-        print(user)
-        print(f"Type of user: {type(user)}")
+    # all_users = user_repository.get_all()
+
+    # for user in all_users:
+    #     print(user)
 
 
 if __name__ == "__main__":
--- entities/repositories/PersonRepository.py
+++ entities/repositories/PersonRepository.py
@@ -1,8 +1,10 @@
+from abc import ABC, abstractmethod
 from typing import List
 from sqlalchemy.orm import Session
+from entities.Person import Person
 
 
-class UserRepository:
+class PersonRepository():
     def __init__(self, session: Session):
         self.session = session
 
@@ -10,20 +12,17 @@ class UserRepository:
         self.session.add(user)
         self.session.commit()
 
-    def add_multiple(self, users: List[User]) -> None:
+    def add_multiple(self, users: List[Person]) -> None:
         self.session.add_all(users)
         self.session.commit()
 
-    def get_by_id(self, id) -> User:
-        return self.session.get(User, id)
+    def get_by_id(self, id) -> Person:
+        return self.session.get(Person, id)
 
-    def get_all(self) -> List[User]:
-        return self.session.query(User).all()
+    def get_all(self) -> List[Person]:
+        return self.session.query(Person).all()
 
     def delete(self, id) -> None:
-        user = self.get(id)
+        user = self.get_by_id(id)
         self.session.delete(user)
         self.session.commit()
-
-
-from entities.User import User
