b775a7a0c9426196f33f2351cb42a173b136b9dc
Mathieu <bon.mathieu@gmail.com>
2024-08-13 22:17:11 +0200

    Code formatting.

--- src/mockup_data/concerts_mock_data.py
+++ src/mockup_data/concerts_mock_data.py
@@ -11,26 +11,21 @@ from src.entities.Show import Show
 
 
 fillmore = Venue(name="The Fillmore", address=None)
+fillmore.address = Address(city="San Francisco", country="USA")
 red_rocks = Venue(name="Red Rocks")
+red_rocks.address = Address(city="Morrison", country="USA")
 hollywood_bowl = Venue(name="Hollywood Bowl")
+hollywood_bowl.address = Address(city="Los Angeles", country="USA")
 msg = Venue(name="Madison Square Garden")
+msg.address = Address(city="New York", country="USA")
 wrigley = Venue(name="Wrigley Field")
 chicago_theatre = Venue(name="Chicago Theatre")
-ewerk = Venue(name="E-Wek")
-
-fillmore.address = Address(city="San Francisco", country="USA")
-red_rocks.address = Address(city="Morrison", country="USA")
-hollywood_bowl.address = Address(city="Los Angeles", country="USA")
-msg.address = Address(city="New York", country="USA")
-ewerk.address = Address(city="Saarbrücken", country="Germany")
 chicago = Address(city="Chicago", country="USA")
 wrigley.address = chicago
 chicago_theatre.address = chicago
-
+ewerk = Venue(name="E-Wek")
+ewerk.address = Address(city="Saarbrücken", country="Germany")
 venues = [fillmore, red_rocks, hollywood_bowl, msg, wrigley, chicago_theatre, ewerk]
-
-
-
 
 nofx_show_artists = [
     Artist(name="Circle Jerks", country="USA"),
@@ -39,13 +34,10 @@ nofx_show_artists = [
     Artist(name="The Meffs", country="England"),
 ]
 
-
-
 nofx_show_attendees = [
     Person(firstname="Franck", lastname="Ludwig"),
     Person(firstname="Agnès", lastname="Maillard"),
 ]
-
 
 nofx_show = Show(
     event_date=datetime(year=2024, month=6, day=1),
@@ -53,11 +45,11 @@ nofx_show = Show(
 )
 
 
-
 for artist in nofx_show_artists:
     concert = Concert()
     concert.artist = artist
     concert.show = nofx_show
-    
+
+nofx_show.name = "NOFX Final Tour"
 nofx_show.venue = ewerk
 nofx_show.attendees = nofx_show_attendees
