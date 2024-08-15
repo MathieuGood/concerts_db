9166cdccdf94a992cf1f5e526997bbd9f2ae351a
Mathieu <bon.mathieu@gmail.com>
2024-08-16 00:44:46 +0200

    Code formatting

--- src/app/routes/show_routes.py
+++ src/app/routes/show_routes.py
@@ -2,13 +2,12 @@ from flask import render_template, request
 from entities.Show import Show
 from repositories.ShowRepository import ShowRepository
 
+
 def register_routes(app, db):
 
     show_repository = ShowRepository(db.session)
 
-    @app.route('/')
+    @app.route("/")
     def all_shows():
         shows = show_repository.get_all()
-        # Return the list_concerts.html template
-        return render_template('list_shows.html', shows=shows)
-        # return str(shows)
+        return render_template("list_shows.html", shows=shows)
