0792e26197377de5be8237bc4d786b6ee47cd9a1
Mathieu <bon.mathieu@gmail.com>
2024-08-16 00:40:21 +0200

    Displaying list of shows in template list_shows.html. FUNCTIONAL.

--- src/app/routes/show_routes.py
+++ src/app/routes/show_routes.py
@@ -4,8 +4,11 @@ from repositories.ShowRepository import ShowRepository
 
 def register_routes(app, db):
 
+    show_repository = ShowRepository(db.session)
+
     @app.route('/')
-    def index():
-        show_repository = ShowRepository(db.session)
+    def all_shows():
         shows = show_repository.get_all()
-        return str(shows)
+        # Return the list_concerts.html template
+        return render_template('list_shows.html', shows=shows)
+        # return str(shows)
