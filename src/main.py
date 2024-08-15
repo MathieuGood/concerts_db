0b84a579410478f87fa1681ed22eab59e5f3c3ab
Mathieu <bon.mathieu@gmail.com>
2024-08-15 23:56:17 +0200

    First working version implementing Flask into the project. Default route prints the content of a Show object. FUNCTIONAL.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from mockup_data.concerts_mock_data import venues, nofx_show
 from db_session import create_session
+from entities.Base import Base
 from entities.Person import Person
 from entities.Artist import Artist
 from entities.Concert import Concert
@@ -19,36 +20,60 @@ from repositories.VideoRepository import VideoRepository
 from repositories.ShowRepository import ShowRepository
 
 
-db = SQLAlchemy()
+def delete_database() -> None:
+    import os
+
+    try:
+        os.remove("src/instance/flask_concerts_db.sqlite")
+        print("*** Database file removed ***")
+    except FileNotFoundError as e:
+        print(e)
+        pass
+
+    print("")
+    print(os.getcwd())
+    print("")
+    input("Press Enter to continue...")
 
 
-def create_app() -> Flask:
+def create_app(db: SQLAlchemy) -> Flask:
     app = Flask(__name__, template_folder="templates")
     app.config["SQLALCHEMY_DATABASE_URI"] = (
         "sqlite+pysqlite:///flask_concerts_db.sqlite"
     )
     db.init_app(app)
 
-    from app.routes.show_routes import register_routes
-    register_routes(app, db)
-
-    migrate = Migrate(app, db)
     return app
 
 
 def main():
-    session = create_session()
 
-    venue_repository = VenueRepository(session)
-    show_repository = ShowRepository(session)
-    person_repository = PersonRepository(session)
+    db = SQLAlchemy(model_class=Base)
+    app = create_app(db)
 
-    venue_repository.add_multiple(venues)
-    show_repository.add(nofx_show)
-    session.commit()
+    with app.app_context():
+        db.create_all()
 
-    first_concert = session.get(Concert, 1)
-    print(first_concert)
+        session = db.session
+
+        from app.routes.show_routes import register_routes
+
+        register_routes(app, db)
+
+        migrate = Migrate(app, db)
+
+        venue_repository = VenueRepository(session)
+        show_repository = ShowRepository(session)
+        person_repository = PersonRepository(session)
+
+        venue_repository.add_multiple(venues)
+        show_repository.add(nofx_show)
+        session.commit()
+
+        first_concert = session.get(Concert, 1)
+        print(first_concert)
+
+    return app
 
 
 if __name__ == "__main__":
--- src/run.py
+++ src/run.py
@@ -1,7 +1,7 @@
-from main import create_app
+from main import main
 
 
-flask_app = create_app()
+flask_app = main()
 
 if __name__ == "__main__":
     flask_app.run(host="0.0.0.0", port=9090, debug=True)
