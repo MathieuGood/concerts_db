a81cc5b3b4875242ae322d1b21ef4f6591690a8d
Mathieu <bon.mathieu@gmail.com>
2024-08-17 00:10:49 +0200

    Attempts to print concerts list in a table but still not functional.

--- src/app/forms/ShowForm.py
+++ src/app/forms/ShowForm.py
@@ -13,10 +13,21 @@ from wtforms.validators import DataRequired
 
 
 class ShowForm(FlaskForm):
+
+    def __init__(self, concerts: list[str], venues: list[str]) -> None:
+        super(ShowForm, self).__init__()
+        self.concerts.choices = concerts
+        self.venue.choices = venues
+
     name = StringField("Name", description="Name of the show")
+    event_date = DateField(
+        "Date", description="Date the event took place", validators=[DataRequired()]
+    )
+
     venue = SelectField(
         "Venue",
         description="Venue where the show took place",
+        choices=[],
         validators=[
             DataRequired(
                 "A venue is needed to create a show, you cannot do without one"
--- src/app/routes/show_routes.py
+++ src/app/routes/show_routes.py
@@ -1,12 +1,16 @@
 from flask import render_template, request
 from entities.Show import Show
 from repositories.ShowRepository import ShowRepository
+from repositories.VenueRepository import VenueRepository
+from repositories.ConcertRepository import ConcertRepository
 from app.forms.ShowForm import ShowForm
 
 
 def register_routes(app, db):
 
     show_repository = ShowRepository(db.session)
+    concert_repository = ConcertRepository(db.session)
+    venue_repository = VenueRepository(db.session)
 
     @app.route("/")
     def all_shows():
@@ -15,5 +19,9 @@ def register_routes(app, db):
 
     @app.route("/create_show")
     def create_show():
-        form = ShowForm()
-        return render_template("edit_show.html", title="Create a new show right now", form=form)
+        concerts = concert_repository.get_all()
+        venues = venue_repository.get_all()
+        form = ShowForm(concerts=concerts, venues=venues)
+        return render_template(
+            "edit_show.html", title="Create a new show right now", form=form
+        )
