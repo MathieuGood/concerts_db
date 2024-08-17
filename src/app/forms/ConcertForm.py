291ab5a29d8d20fd55c4a3caabe49ca8208656fb
Mathieu <bon.mathieu@gmail.com>
2024-08-18 00:43:42 +0200

    Updated README with install and run instructions.

--- src/app/forms/ConcertForm.py
+++ src/app/forms/ConcertForm.py
@@ -1,12 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import (
-    StringField,
-    PasswordField,
-    BooleanField,
     SelectField,
     SelectMultipleField,
     SubmitField,
-    DateField,
     TextAreaField,
 )
 from wtforms.validators import DataRequired
