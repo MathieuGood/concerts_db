47c44c0efbe1b98cd22c0dea6588dddff6ca742c
Mathieu <bon.mathieu@gmail.com>
2024-08-18 00:06:23 +0200

    Removed file used for test.

---
+++ src/app/forms/ConcertsForm.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SelectField,
+    SelectMultipleField,
+    SubmitField,
+    DateField,
+    TextAreaField,
+)
+
+
+class Concerts(FlaskForm):
+    pass
