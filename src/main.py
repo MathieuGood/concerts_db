f06deb59b997590b015c04a1a815e8dc48884667
Mathieu <bon.mathieu@gmail.com>
2024-10-05 15:22:22 +0200

    Code formatting.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from database.database import drop_and_recreate_all_tables, seed_data
-from models.base import Base
 from config import Config
 from routes.root import router as root_router
 from routes.address import router as address_router
@@ -21,13 +20,12 @@ from routes.video import router as video_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     engine: Engine = create_engine(Config.DATABASE_URI, echo=True)
-
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db: Session = SessionLocal()
 
     if Config.DEMO_MODE:
         print("Running in demo mode")
-        drop_and_recreate_all_tables(engine)
+        drop_and_recreate_all_tables(engine=engine)
         seed_data(db)
 
     db.close()
