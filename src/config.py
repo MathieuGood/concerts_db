c4aa7e84cdb0e9b1ddd865a610e5d40584779655
Mathieu <bon.mathieu@gmail.com>
2024-10-05 14:54:38 +0200

    Removed unused SECRET_KEY from .env

--- src/config.py
+++ src/config.py
@@ -5,6 +5,5 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
     DATABASE_URI: str = os.getenv("DATABASE_URI")
     DEMO_MODE: bool = os.getenv("DEMO_MODE", "False").lower() in ("true", "1", "t")
