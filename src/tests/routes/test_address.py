1d6b7071ecbef331931406b0c57d34236bedc848
Mathieu <bon.mathieu@gmail.com>
2024-10-06 19:57:37 +0200

    Setting up architecture for tests.

---
+++ src/tests/routes/test_address.py
@@ -0,0 +1,14 @@
+def test_create_address(client):
+    response = client.post(
+        "/address/",
+        json={
+            "city": "Springfield",
+            "country": "USA",
+        },
+    )
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["city"] == "Springfield"
+    assert response_data["country"] == "USA"
+    assert "id" in response_data
+
