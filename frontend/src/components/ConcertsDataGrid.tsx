69f2e8137b77ce4a63791e78bd80a30b905c0369
Mathieu <bon.mathieu@gmail.com>
2024-11-15 10:46:52 +0100

    Artist selector onChange functionnal : correctly updating show object.

--- frontend/src/components/ConcertsDataGrid.tsx
+++ frontend/src/components/ConcertsDataGrid.tsx
@@ -1,40 +1,57 @@
 import { Show } from "../models/Show"
 import { Artist } from "../models/Artist"
-import { DataGrid, GridColDef, GridRenderEditCellParams, GridRowsProp } from "@mui/x-data-grid"
+import {
+	DataGrid,
+	GridColDef,
+	GridRenderEditCellParams,
+	GridRowsProp,
+	GridCellEditStopParams
+} from "@mui/x-data-grid"
 import { MenuItem, Select } from "@mui/material"
+import { Concert } from "../models/Concert"
 
 const ConcertDataGrid: React.FC<{
 	show: Show
 	setShow: (show: Show) => void
 	artists: Artist[]
 }> = ({ show, setShow, artists }) => {
-	const handleArtistChange = (concertId: number, artistId: string) => {
-		// Not implemented yet
-		console.log(`Change the artist to concertId < ${concertId} > and artistId < ${artistId} >`)
-		
+	const handleArtistChange = (rowIndex: number, artistId: number) => {
+		const updatedConcerts = show.concerts.map((concert, index) => {
+			console.log(concert)
+			if (index === rowIndex) {
+				return { ...concert, artist: artists.find(artist => artist.id === artistId) }
+			}
+			return concert
+		})
+		setShow({ ...show, concerts: updatedConcerts })
 	}
 
-	const renderArtistCell = (index: number) => {
-		const concert = show.concerts[index]
-		console.log("UNDEFINED CONCERTS?", show.concerts)
-		console.log("UNDEFINED ARTIST?", concert)
-		console.log(`Render artistCell with index ${index}: ${concert.artist}`)
-		if (!concert) {
-			return <span> NO CONCERT</span>
-		}
+	const handleCellEditCommit = (params: GridCellEditStopParams) => {
+		const { id, field, value } = params
+		const updatedConcerts = show.concerts.map(concert => {
+			if (concert.id === id) {
+				return { ...concert, [field]: value }
+			}
+			return concert
+		})
+		setShow({ ...show, concerts: updatedConcerts })
+	}
+
+	const renderArtistCell = (params: GridRenderEditCellParams) => {
+		const concert = params.row as Concert
+		const rowNumber = params.row.rowIndex
 		return (
 			<Select
-				value={concert.artist != undefined && artists.length > 0 ? concert.artist.id : ""}
+				value={
+					concert.artist?.id != undefined && artists.length > 0 ? concert.artist.id : ""
+				}
 				onChange={event => {
-					console.log("Artist CHANGE :", index, event.target.value)
-					// handleArtistChange(index, event.target.value)
+					handleArtistChange(rowNumber, Number(event.target.value))
 				}}
 				fullWidth
 				variant="standard">
 				{artists.map(artist => (
-					<MenuItem key={artist.id} value={artist.id}>
-						{artist.name}
-					</MenuItem>
+					<MenuItem value={artist.id}>{artist.name}</MenuItem>
 				))}
 			</Select>
 		)
@@ -46,33 +63,28 @@ const ConcertDataGrid: React.FC<{
 			field: "artist",
 			headerName: "Artist",
 			width: 250,
-			renderCell: params => {
-				renderArtistCell
-			},
-			renderEditCell: (params:GridRenderEditCellParams) => {
-                
-            }
+			renderCell: params => renderArtistCell(params)
 		},
-		{ field: "comments", headerName: "Comments", minWidth: 150, maxWidth: 300 },
+		{ field: "comments", headerName: "Comments", minWidth: 200, maxWidth: 300, editable: true },
 		{ field: "photos", headerName: "Photos", width: 20 },
 		{ field: "videos", headerName: "Videos", width: 20 },
-		{ field: "setlist", headerName: "Setlist", minWidth: 150, maxWidth: 300 }
+		{ field: "setlist", headerName: "Setlist", minWidth: 150, editable: true }
 	]
 
-	const rows: GridRowsProp = show.concerts.map(concert => {
+	const rows: GridRowsProp = show.concerts.map((concert, index) => {
 		const concertRows = {
 			id: concert.id || 0,
-			artist: concert.artist?.name,
+			rowIndex: index,
+			artist: concert.artist,
 			comments: concert.comments,
 			photos: concert.photos.length,
 			videos: concert.videos.length,
 			setlist: concert.setlist
 		}
-		console.log("concertRows", concertRows)
 		return concertRows
 	})
 
-	return <DataGrid columns={columns} rows={rows} />
+	return <DataGrid columns={columns} rows={rows} onCellEditCommit={handleCellEditCommit} />
 }
 
 export default ConcertDataGrid
